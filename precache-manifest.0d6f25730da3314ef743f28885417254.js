393419de03fa3324273221c290fa1e7834e2837c
Sahil Bhanvadiya <180120107008@git.org.in>
2021-11-17 11:35:48 +0530

    Updates

---
+++ precache-manifest.0d6f25730da3314ef743f28885417254.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f1fbb742d33183d66611f698c243f0",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "0ce6078b1746561fc872",
+    "url": "/myportfolio/static/css/main.ea301a12.chunk.css"
+  },
+  {
+    "revision": "93dc5d7eee458cd0f352",
+    "url": "/myportfolio/static/js/2.32861029.chunk.js"
+  },
+  {
+    "revision": "f3a61d2d98728a54568d625465fb46b3",
+    "url": "/myportfolio/static/js/2.32861029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce6078b1746561fc872",
+    "url": "/myportfolio/static/js/main.116158e8.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  },
+  {
+    "revision": "7f2490e6e1bf4f7c830dd713a1b72967",
+    "url": "/myportfolio/static/media/logo.7f2490e6.png"
+  },
+  {
+    "revision": "3b94ed8db34e8e337a27caa59dac08ce",
+    "url": "/myportfolio/static/media/pfp.3b94ed8d.png"
+  },
+  {
+    "revision": "646243d7034c314ca5d53dd710e895da",
+    "url": "/myportfolio/static/media/scroll-down.646243d7.svg"
+  }
+]);
